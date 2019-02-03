91a9921c87fc1e06d34fa1e0d53bd8ce2c35330f
diegoajv <diegojosevilla@outlook.com>
2019-02-03 00:19:24 -0600

    Feat: Add login view to index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var site_lang = 'es';
+
+var login_form = {
+  'es': {
+    title_label: 'INICIAR ',
+    username_label: 'Usuario',
+    password_label: 'Contraseña',
+    login_button: 'Entrar'
+  }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { login_form: login_form[site_lang] });
 });
 
 module.exports = router;
