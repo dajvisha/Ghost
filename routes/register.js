6a889e85b39fc5abda8df742224a4d6cd9eb6cdc
diegoajv <diegojosevilla@outlook.com>
2019-02-03 22:05:35 -0600

    Feat: Add bcrypt to hash password

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 var international = require('../resources/international');
 
 var labels = international.labels[international.language];
+const saltRounds = 10;
 
 /* POST dash. */
 router.get('/', function(req, res, next) {
@@ -10,10 +12,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body.name);
-  console.log(req.body.email);
-  console.log(req.body.pass);
-  console.log(req.body.passconf);
+  var name = req.body.name;
+  var email = req.body.email;
+  var pass = req.body.pass;
+  var passconf = req.body.passconf;
+
+  bcrypt.hash(pass, saltRounds, function(err, pass_hash) {
+    console.log(pass_hash);
+  });
 
   res.render('register', { register_form: labels.register_form });
 });
